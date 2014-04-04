8329c39c8f5f514b2125dbdc9f2bedf53d871236
pirotess8330 <ilia.c.lyons@gmail.com>
2014-04-03 21:57:51 -0700

    First Update

--- gen/slaphappy/v1/R.java
+++ gen/slaphappy/v1/R.java
@@ -38,6 +38,7 @@ public final class R {
         public static final int ball=0x7f020000;
         public static final int cat=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int settings=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f0b0000;
--- src/slaphappy/v1/MainActivity.java
+++ src/slaphappy/v1/MainActivity.java
@@ -3,7 +3,9 @@ package slaphappy.v1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,10 +15,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
 		setContentView(R.layout.activity_main);
 	}
 
@@ -24,7 +22,25 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		// Inflate settings button menu if present
+		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.menu_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		case R.id.action_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 	/** Called when the user clicks the Let's Play button */
